c2458a2a7f09e5ce0e4861bf7f3dcf646783fa77
Tinsae Shemalise <tinsaeshemalise41@gmail.com>
2024-11-03 17:04:38 +0300

    Feat: Performed seasonality analysis

--- scripts/time_sereies_analyzer.py
+++ scripts/time_sereies_analyzer.py
@@ -12,11 +12,29 @@ class TimeSeriesAnalyzer:
         self.data = time_series_data
         self.date_col = date_column
     
-    def visualize_value(self, col : str, label: str , title: str):
+    def analyze_trend(self, col : str, label: str , title: str):
         plt.plot(self.data[self.date_col], self.data[col], label=label, color='blue')
         plt.title(title)
         plt.xlabel('Date')
         plt.ylabel(col)
         # plt.axvline(x='Date', color='red', linestyle='--', label='Event Marker')
         plt.legend()
+        plt.show()
+    
+    def analyze_seasonality(self):
+        # Extract the year from the Date
+        self.data['Year'] = self.data['Date'].dt.year
+
+        # Calculate average price per year
+        yearly_avg = self.data.groupby('Year')['Price'].mean().reset_index()
+
+        # Plot yearly average prices
+        plt.figure(figsize=(12, 4))
+        sns.barplot(x='Year', y='Price', data=yearly_avg, hue='Year', legend=False, palette='husl')
+        plt.title('Average Yearly Oil Prices', fontsize=16)
+        plt.xlabel('Year', fontsize=14)
+        plt.ylabel('Average Price', fontsize=14)
+        plt.xticks(rotation=45)
+        plt.grid(axis='y')
+        plt.tight_layout()
         plt.show()
