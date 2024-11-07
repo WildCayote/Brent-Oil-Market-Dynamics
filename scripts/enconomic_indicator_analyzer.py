be42e374613ebf0ad754efb281785fd633da125e
Tinsae Shemalise <tinsaeshemalise41@gmail.com>
2024-11-07 17:32:31 +0300

    Feat: Performed economic indicator analysis

--- scripts/enconomic_indicator_analyzer.py
+++ scripts/enconomic_indicator_analyzer.py
@@ -1,5 +1,7 @@
 import wbdata
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 class Analyzer:
     @staticmethod
@@ -25,3 +27,35 @@ class Analyzer:
         data_daily.reset_index(inplace=True)
         data_daily.rename(columns={'index': 'Date'}, inplace=True)
         return data_daily
+
+    @staticmethod
+    def analyz_and_plot(indicator_data, indicator_name, oil_data, x_label, color):
+        merged_data = pd.merge(indicator_data, oil_data.reset_index(), on='Date')
+        
+        # Drop NaN values to ensure correlation calculation is valid
+        merged_data.dropna(inplace=True)
+        correlation = merged_data[indicator_name].corr(merged_data['Price'])
+        print(f"Correlation between {indicator_name} and oil prices: {correlation}")
+
+        # Scatter plot
+        plt.figure(figsize=(10, 4))
+        sns.scatterplot(data=merged_data, x=indicator_name, y='Price', color=color)
+        plt.title(f'{indicator_name} vs Brent Oil Prices')
+        plt.xlabel(x_label)
+        plt.ylabel('Brent Oil Price ($)')
+        plt.show()
+
+    @staticmethod
+    def analyz_indicators(gdp_data, inflation_data, unemployment_data, exchange_rate_data, oil_data):
+        
+        # Analyze GDP
+        Analyzer.analyz_and_plot(gdp_data, 'GDP', oil_data, 'GDP Growth Rate (%)', color='SteelBlue')
+
+        # Analyze Inflation
+        Analyzer.analyz_and_plot(inflation_data, 'CPI', oil_data, 'Inflation Rate (%)', color='DarkOrange')
+
+        # Analyze Unemployment
+        Analyzer.analyz_and_plot(unemployment_data, 'Unemployment Rate', oil_data, 'Unemployment Rate (%)', color='SeaGreen')
+
+        # Analyze Exchange Rate
+        Analyzer.analyz_and_plot(exchange_rate_data, 'Exchange Rate', oil_data, 'Exchange Rate (USD to Local Currency)', color='FireBrick')
